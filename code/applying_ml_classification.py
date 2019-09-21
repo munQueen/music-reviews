14ce90820062e58e6aca4a388bd12fc753fbb959
munQueen <49634960+munQueen@users.noreply.github.com>
2019-09-21 10:53:38 -0700

    Add files via upload

--- code/applying_ml_classification.py
+++ code/applying_ml_classification.py
@@ -2,24 +2,26 @@ import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
 from sklearn.preprocessing import StandardScaler
-#import category_encoders as ce
-from sklearn.svm import LinearSVC, SVC
-from sklearn.metrics import confusion_matrix, classification_report 
+import category_encoders as ce
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+import seaborn as sns
 
-
-#os.chdir('C:\Learning\ML\spotify')
-os.chdir('C:\ML\Springboard\music-reviews-master')
+os.chdir('C:\Learning\ML\spotify')
 full_data = pd.read_csv('full_data.csv').dropna()
 
-#remove all albums which are given multiple genres 
+# remove all albums which are given multiple genres 
 single_genre_albums = full_data['rap'].astype(int) + full_data['electronic'].astype(int) +\
    full_data['experimental'].astype(int) + full_data['folk/country'].astype(int) +\
    full_data['global'].astype(int) + full_data['jazz'].astype(int) +\
    full_data['metal'].astype(int) + full_data['pop/r&b'].astype(int) +\
    full_data['rock'].astype(int) == 1
 
+# create a 'genre' column 
 data = full_data[single_genre_albums].dropna()
 data['genre'] = ''
 data.loc[data['rap']== True, 'genre'] = 'rap'
@@ -36,14 +38,9 @@ data.genre.value_counts()
 
 #transformations:
 #drop the bottom genres, resample from the top genre for balanced classes
-data.genre.value_counts() #/ len(data.genre)
+print(data.genre.value_counts() )
 
-#completely drop jazz and global
-#consider dropping metal
-
-#definitely resample rock
-#consider resampling electronic, rap
-
+# best options appear to be either keeping rock/electronic and resampling to 1000, or rock/electronic/rap/pop and resampling to 500
 #resampling top 2 to 1000
 rock_sample = data[data.genre == 'rock'].sample(n=1000, random_state=42)
 electronic_sample = data[data.genre == 'electronic'].sample(n=1000, random_state=42)
@@ -58,26 +55,23 @@ pop_sample = data[data.genre == 'pop/r&b'].sample(n=500, random_state=42)
 resampled_500 = rock_sample.append(electronic_sample).append(rap_sample).append(pop_sample)
 
 
+# using the resampled to 500 data
+data = resampled_500
+
 #set up all of the variables 
 regression_columns = ['danceability_mean', 'energy_mean', 'loudness_mean', 'speechiness_mean', 'acousticness_mean', 
                       'instrumentalness_mean', 'liveness_mean', 'valence_mean', 'tempo_mean', 
                       'danceability_std', 'energy_std', 'loudness_std', 'speechiness_std', 'acousticness_std', 
                       'instrumentalness_std', 'liveness_std', 'valence_std', 'tempo_std', 'duration_ms_mean', 'key_max_pct']
 
+# prepare the categorical variables
+data['primary_time'] = data['primary_time'].apply(str)
+ohe = ce.OneHotEncoder(handle_unknown='ignore', use_cat_names=True)
+categorical_columns = data[['primary_key', 'primary_mode', 'primary_time']]
+categorical_data = ohe.fit_transform(categorical_columns)
+#for drop 1 of each category
+categorical_data = categorical_data.drop(columns=['primary_key_C', 'primary_mode_Major', 'primary_time_4.0'])
 
-data = resampled_1000
-
-def airplane_stuff():
-    data['primary_time'] = data['primary_time'].apply(str)
-    ohe = ce.OneHotEncoder(handle_unknown='ignore', use_cat_names=True)
-    categorical_columns = data[['primary_key', 'primary_mode', 'primary_time']]
-    #prepare the categorical variables
-    ohe = ce.OneHotEncoder(handle_unknown='ignore', use_cat_names=True)
-    
-    categorical_data = ohe.fit_transform(categorical_columns)
-    #for linear regression: drop 1 of each category
-    categorical_data = categorical_data.drop(columns=['primary_key_C', 'primary_mode_Major', 'primary_time_4.0'])
-    return()
 
 #prepare the numeric variables
 numeric_features = data[regression_columns]
@@ -85,45 +79,117 @@ scaler = StandardScaler()
 numeric_scaled = scaler.fit_transform(numeric_features)
 numeric_scaled = pd.DataFrame(numeric_scaled)
 numeric_scaled.columns = regression_columns
-#X = pd.concat([numeric_scaled.reset_index(), categorical_data.reset_index()], axis=1).drop(columns=['index'])
-X = numeric_scaled
+X = pd.concat([numeric_scaled.reset_index(), categorical_data.reset_index()], axis=1).drop(columns=['index'])
 y = data.genre
 
 
-X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=0.3)
-#classifier = SVC(random_state=0, tol=1e-4, max_iter=8000)
-#classifier.fit(X_train, y_train)
-#classifier.score(X_train, y_train)
+X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=0.4)
 
-clf = SVC(random_state=0, kernel='rbf', C=1)
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-cr = classification_report(y_test, y_pred, output_dict=True)
-print("Accuracy on training data:", clf.score(X_train, y_train))
-print("Accuracy on testing data:", clf.score(X_test, y_test))
-print('Micro avg score on test data:', cr['micro avg']['f1-score'])
-
-#to look up: 
-#ROC curve w/multiple classes? 
-
-
+def metric_aggregation(model, X, y):
+   # this function will be used to take the various models, and store output from classification_report() 
+   # after running this for our three models, we can compare their performance in a variety of ways
+   true_data = y
+   predicted_data = model.predict(X)
+   cr = classification_report(true_data, predicted_data, output_dict=True)
+   avg_f1 = cr['weighted avg']['f1-score']
+   avg_precision = cr['weighted avg']['precision']
+   avg_recall = cr['weighted avg']['recall']
+   return([avg_f1, avg_precision, avg_recall])
+   
+   
+#Start running the three models
 params = [
-      {'C':[1, 10, 100], 'kernel':['linear', 'rbf', 'poly'], 
-       'gamma':[0.001, 0.0001, 0.00001], 'degree':[2, 3, 4, 5]}
-      ]
+      {'C':[0.01, 0.1, 1, 10, 100], 
+       'kernel':['rbf'], 
+       'gamma':[0.001, 0.01, 0.1, 1]
+      }]
 
-model = GridSearchCV(estimator=SVC(),
+svc_model = GridSearchCV(estimator=SVC(random_state=42),
+                     param_grid=params,
+                     cv=5
+                     )
+
+svc_model.fit(X_train, y_train)
+print(svc_model.best_params_)
+
+
+svc_train_metrics = metric_aggregation(svc_model, X_train, y_train)
+svc_test_metrics = metric_aggregation(svc_model, X_test, y_test)
+
+
+
+#running KNN 
+params = [
+      {'n_neighbors':[1, 2, 3, 4, 5, 6, 7], 
+       'p':[1, 2, 3]
+      }]
+knn_model = GridSearchCV(estimator=KNeighborsClassifier(),
                      param_grid=params,
                      cv=5)
+knn_model.fit(X_train, y_train)
 
-model.fit(X_train, y_train)
+knn_train_metrics = metric_aggregation(knn_model, X_train, y_train)
+knn_test_metrics = metric_aggregation(knn_model, X_test, y_test)
 
-clf = SVC(random_state=42, kernel='rbf', C=100, gamma=0.001)
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-cr = classification_report(y_test, y_pred, output_dict=True)
-print("Accuracy on training data:", clf.score(X_train, y_train))
-print("Accuracy on testing data:", clf.score(X_test, y_test))
-print('Micro avg score on test data:', cr['micro avg']['f1-score'])
-cr2 = classification_report(y_test, y_pred)
 
+
+
+# calibrating the random forest classifier
+n_estimators = [int(x) for x in np.linspace(start = 50, stop = 500, num = 10)]
+max_features = ['auto', 'sqrt']
+max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+max_depth.append(None)
+min_samples_split = [2, 5, 10]
+min_samples_leaf = [1, 2, 4]
+bootstrap = [True, False]
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap}
+
+
+rf = RandomForestClassifier(random_state=42)
+rfc_model = RandomizedSearchCV(estimator=rf,
+                               param_distributions=random_grid,
+                               n_iter=50,
+                               cv=10,
+                               verbose=10)
+rfc_model.fit(X_train, y_train)
+y_pred = rfc_model.predict(X_test)
+
+rfc_test_metrics = metric_aggregation(rfc_model, X_test, y_test)   
+rfc_train_metrics = metric_aggregation(rfc_model, X_train, y_train) 
+
+rfc_params = rfc_model.best_params_
+
+
+# set up the output 
+model_col = ['SVC Test'] * 3  + ['SVC Train'] * 3 +  ['KNN Test'] * 3 + ['KNN Train'] * 3  + ['RFC Test'] * 3 + ['RFC Train'] * 3
+metric_col = ['F1', 'Precision', 'Recall'] * 6
+data = svc_test_metrics + svc_train_metrics + knn_test_metrics + knn_train_metrics + rfc_test_metrics + rfc_train_metrics
+
+metric_dict = {'Model':model_col, 'Metric':metric_col, 'Data':data}
+metric_df = pd.DataFrame(metric_dict)
+
+
+plt.figure()
+sns.barplot(hue='Metric', x='Model', y='Data', data=metric_df.loc[(metric_df.Model == 'SVC Test') | (metric_df.Model == 'KNN Test')| (metric_df.Model == 'RFC Test')])
+plt.title("Comparison of classification models - Testing data")
+plt.xlabel("Model")
+plt.ylabel("Value of metrics")
+plt.show()
+
+plt.figure()
+sns.barplot(hue='Metric', x='Model', y='Data', data=metric_df.loc[(metric_df.Model == 'SVC Train') | (metric_df.Model == 'KNN Train')| (metric_df.Model == 'RFC Train')])
+plt.title("Comparison of classification models - Training data")
+plt.xlabel("Model")
+plt.ylabel("Value of metrics")
+plt.show()
+
+#look at the confusion matrix for SVC and RFC
+cm = confusion_matrix(y_test, rfc_model.predict(X_test))
+cm_df = pd.DataFrame(cm)
+cm_df.columns = ['Predicted Electronic', 'Predicted Pop/R&B', 'Predicted Rap', 'Predicted Rock']
+cm_df.index = ['True Electronic', 'True Pop/R&B', 'True Rap', 'True Rock']
--- code/applying_ml_ols.py
+++ code/applying_ml_ols.py
@@ -4,31 +4,27 @@
 Created on Thu Aug 15 19:04:48 2019
 @author: jwulz
 """
-
+# import all of the packages needed
 import pandas as pd
 import numpy as np
 import os
-import scipy.stats
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
 import category_encoders as ce
-
+import seaborn as sns
 import statsmodels.api as sm
 from statsmodels.formula.api import ols
+
 
 os.chdir('C:\Learning\ML\spotify')
 full_data = pd.read_csv('full_data.csv').dropna()
 
 
-#how to transform the score variable?
-score_sq = full_data.score ** 2
-
-
-X = full_data.drop(['review_id', 'album_uri_to_use', 'score', 'artist_name', 'album_name', 'publication_year', 'release_year', 'missing'], axis=1)
-#set up all of the variables 
+################X = full_data.drop(['review_id', 'album_uri_to_use', 'score', 'artist_name', 'album_name', 'publication_year', 'release_year', 'missing'], axis=1)
+# list the numeric variables here
 regression_columns = ['danceability_mean', 'energy_mean', 'loudness_mean', 'speechiness_mean', 'acousticness_mean', 
                       'instrumentalness_mean', 'liveness_mean', 'valence_mean', 'tempo_mean', 
                       'danceability_std', 'energy_std', 'loudness_std', 'speechiness_std', 'acousticness_std', 
@@ -40,34 +36,41 @@ ohe = ce.OneHotEncoder(handle_unknown='ignore', use_cat_names=True)
 
 #set up a function to run this model on each genre of music separately 
 def genre_linreg(gname, visualize_residuals=False):
+   # keep only data pertaining to the given genre
    subset_df = full_data[full_data[gname] == True]
    categorical_columns = subset_df[['primary_key', 'primary_mode', 'primary_time']]
-   #prepare the categorical variables
-   ohe = ce.OneHotEncoder(handle_unknown='ignore', use_cat_names=True)
    
+   # prepare the categorical variables
+   ohe = ce.OneHotEncoder(handle_unknown='ignore', use_cat_names=True)
    categorical_data = ohe.fit_transform(categorical_columns)
-   #for linear regression: drop 1 of each category
+   # for linear regression dummy variables: drop 1 of each category
    categorical_data = categorical_data.drop(columns=['primary_key_C', 'primary_mode_Major', 'primary_time_4.0'])
    
-   #prepare the numeric variables
+   # prepare the numeric variables
    numeric_features = subset_df[regression_columns].dropna()
    scaler = StandardScaler()
    numeric_scaled = scaler.fit_transform(numeric_features)
    numeric_scaled = pd.DataFrame(numeric_scaled)
    numeric_scaled.columns = regression_columns
    X = pd.concat([numeric_scaled.reset_index(), categorical_data.reset_index()], axis=1).drop(columns=['index'])
+   X = sm.add_constant(X)
    y = list(subset_df.score ** 2)
-   X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=0.2)
-   X_train = sm.add_constant(X_train)
+   
+   # split X and y into test and train sets
+   X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=0.4)
+   
+   # fit OLS
    genre_linreg = sm.OLS(y_train, X_train).fit()
    print("Linear regression for genre:", gname)
    print(genre_linreg.summary())
    
-   data = {'fitted_values':genre_linreg.fittedvalues, 'residuals':genre_linreg.resid}
-   residuals = pd.DataFrame(data)
-   np.random.seed(42)
-   
+   # code to optionally visualize the residuals
    if visualize_residuals == True:
+   
+      data = {'fitted_values':genre_linreg.fittedvalues, 'residuals':genre_linreg.resid}
+      residuals = pd.DataFrame(data)
+      np.random.seed(42)
+   
       if residuals.fitted_values.shape[0] < 250:
          subset = residuals
       else:
@@ -79,6 +82,7 @@ def genre_linreg(gname, visualize_residuals=False):
       
    return(X_train, X_test, y_train, y_test, genre_linreg)
 
+# run the function for each genre
 rap_X_train, rap_X_test, rap_y_train, rap_y_test, rap_linreg = genre_linreg('rap')
 electronic_X_train, electronic_X_test, electronic_y_train, electronic_y_test, electronic_linreg = genre_linreg('electronic')
 experimental_X_train, experimental_X_test, experimental_y_train, experimental_y_test, experimental_linreg = genre_linreg('experimental')
@@ -89,40 +93,90 @@ metal_X_train, metal_X_test, metal_y_train, metal_y_test, metal_linreg = genre_l
 pop_X_train, pop_X_test, pop_y_train, pop_y_test, pop_linreg = genre_linreg('pop/r&b')
 rock_X_train, rock_X_test, rock_y_train, rock_y_test, rock_linreg = genre_linreg('rock')
 
-
-#best R2: metal (.11)
-#jazz (.10)
-#electronic
-#rock 
-
-
-def rfr_model(X, y):
-   #grid search
-   gsc = GridSearchCV(estimator=RandomForestRegressor(), 
-                      param_grid={
-                            'max_depth':range(3,7),
-                            'n_estimators':(10,50,100,1000), 
-                            }, 
-                            cv=5, scoring='neg_mean_squared_error', verbose=0, n_jobs=-1)
-   
-   grid_result = gsc.fit(X, y)
-   best_params = grid_result.best_params_
-   
-   rfr = RandomForestRegressor(max_depth=best_params["max_depth"],
-                               n_estimators=best_params["n_estimators"], 
-                               random_state=False, verbose=False)
-   
-   #perform K-fold CV
-   scores = cross_val_score(rfr, X, y, cv=10, scoring='neg_mean_absolute_error')
-   predictions = cross_val_predict(rfr, X, y, cv=10)
-   rfr2 = RandomForestRegressor(max_depth=best_params["max_depth"],
-                               n_estimators=best_params["n_estimators"], 
-                               random_state=False, verbose=False)
-   rfr2.fit(X, y)
-   return scores, predictions, rfr2
+# calculate the MSE
+ols_rap_train_mse = mean_squared_error(rap_y_train, rap_linreg.predict(rap_X_train))
+ols_rap_test_mse = mean_squared_error(rap_y_test, rap_linreg.predict(rap_X_test))
+ols_rock_train_mse = mean_squared_error(rock_y_train, rock_linreg.predict(rock_X_train))
+ols_rock_test_mse = mean_squared_error(rock_y_test, rock_linreg.predict(rock_X_test))
+ols_electronic_train_mse = mean_squared_error(electronic_y_train, electronic_linreg.predict(electronic_X_train))
+ols_electronic_test_mse = mean_squared_error(electronic_y_test, electronic_linreg.predict(electronic_X_test))
+ols_pop_train_mse = mean_squared_error(pop_y_train, pop_linreg.predict(pop_X_train))
+ols_pop_test_mse = mean_squared_error(pop_y_test, pop_linreg.predict(pop_X_test))
 
 
-metal_scores, metal_predictions, metal_rfr = rfr_model(metal_X_train, metal_y_train)
 
-metal_rfr.feature_importances_
-metal_mse = sum((metal_predictions - metal_y_train) ** 2) / len(metal_predictions)
+
+
+
+
+from sklearn.model_selection import RandomizedSearchCV
+# set up the combinations of parameters
+n_estimators = [int(x) for x in np.linspace(start = 100, stop = 1000, num = 10)]
+max_features = ['auto', 'sqrt']
+max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+max_depth.append(None)
+min_samples_split = [2, 5, 10]
+min_samples_leaf = [1, 2, 4]
+bootstrap = [True, False]
+
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap}
+
+#run the grid search random forest regressors
+rf = RandomForestRegressor(random_state=42)
+rfr_rap = RandomizedSearchCV(estimator=rf, param_distributions = random_grid, n_iter=50, cv=5, verbose=10)
+rfr_rap.fit(rap_X_train, rap_y_train)
+
+
+rfr_rock = RandomizedSearchCV(estimator=rf, param_distributions = random_grid, n_iter=50, cv=5, verbose=10)
+rfr_rock.fit(rock_X_train, rock_y_train)
+#started at 10:25pm
+
+rfr_electronic = RandomizedSearchCV(estimator=rf, param_distributions = random_grid, n_iter=50, cv=5, verbose=10)
+rfr_electronic.fit(electronic_X_train, electronic_y_train)
+
+
+rfr_pop = RandomizedSearchCV(estimator=rf, param_distributions = random_grid, n_iter=50, cv=5, verbose=10)
+rfr_pop.fit(pop_X_train, pop_y_train)
+
+
+rfr_rap_train_mse = mean_squared_error(rap_y_train, rfr_rap.predict(rap_X_train))
+rfr_rap_test_mse = mean_squared_error(rap_y_test, rfr_rap.predict(rap_X_test))
+rfr_rock_train_mse = mean_squared_error(rock_y_train, rfr_rock.predict(rock_X_train))
+rfr_rock_test_mse = mean_squared_error(rock_y_test, rfr_rock.predict(rock_X_test))
+rfr_electronic_train_mse = mean_squared_error(electronic_y_train, rfr_electronic.predict(electronic_X_train))
+rfr_electronic_test_mse = mean_squared_error(electronic_y_test, rfr_electronic.predict(electronic_X_test))
+rfr_pop_train_mse = mean_squared_error(pop_y_train, rfr_pop.predict(pop_X_train))
+rfr_pop_test_mse = mean_squared_error(pop_y_test, rfr_pop.predict(pop_X_test))
+
+#create a dataframe to compare the different MSEs for the four genres
+
+
+ols_tests = [ols_rap_test_mse, ols_rock_test_mse, ols_electronic_test_mse, ols_pop_test_mse]
+ols_train = [ols_rap_train_mse, ols_rock_train_mse, ols_electronic_train_mse, ols_pop_train_mse]
+rfr_tests = [rfr_rap_test_mse, rfr_rock_test_mse, rfr_electronic_test_mse, rfr_pop_test_mse]
+rfr_train = [rfr_rap_train_mse, rfr_rock_train_mse, rfr_electronic_train_mse, rfr_pop_train_mse]
+
+genre_col = ['rap'] * 4 + ['rock'] * 4 + ['electronic'] * 4 + ['pop'] * 4
+model_col = ['OLS Test', 'OLS Train', 'RFR Test', 'RFR Train'] * 4
+mse_col = ols_tests + ols_train + rfr_tests + rfr_train
+data = {'Genre':genre_col, 'Model':model_col, 'MSE':mse_col}
+model_comparison = pd.DataFrame(data)
+
+plt.figure()
+sns.barplot(hue='Model', x='Genre', y='MSE', data=model_comparison.loc[(model_comparison.Model == 'OLS Test') | (model_comparison.Model == 'RFR Test')])
+plt.title("Comparison of OLS vs RFR - Testing data")
+plt.xlabel("Genre")
+plt.ylabel("MSE (smaller is better)")
+plt.show()
+
+plt.figure()
+sns.barplot(hue='Model', x='Genre', y='MSE', data=model_comparison.loc[(model_comparison.Model == 'OLS Train') | (model_comparison.Model == 'RFR Train')])
+plt.title("Comparison of OLS vs RFR - Training data")
+plt.xlabel("Genre")
+plt.ylabel("MSE (smaller is better)")
+plt.show()
